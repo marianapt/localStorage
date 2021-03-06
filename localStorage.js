27d6f00814d69c44ef879dbd70ebe1bbc9f5ee6e
mperez <mariana.perez@q2.com>
2021-03-06 14:20:17 -0600

    first commit

---
+++ localStorage.js
@@ -0,0 +1,13 @@
+localStorage.setItem('name', 'Manny');
+
+localStorage.setItem('name2', 'emmanuel')
+
+localStorage.removeItem('name2'); 
+
+
+const name = localStorage.getItem('name');
+
+const h1 = document.querySelector('#title');
+
+name ? h1.textContent = `Welcome ${name}` : h1.textContent = 'So sad nobody to welcome';
+
